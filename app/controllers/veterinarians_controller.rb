fcf7bbed129cb0d9813fed1ed3724316126ca892
Justin Pacubas <jpacubas@gmail.com>
2014-09-13 15:37:04 -0700

    Refactor Veterinarians controller to use implicit rendering.

--- app/controllers/veterinarians_controller.rb
+++ app/controllers/veterinarians_controller.rb
@@ -2,43 +2,39 @@ class VeterinariansController < ApplicationController
   def index
     @veterinarians = Veterinarian.all
     @veterinarian = Veterinarian.new
-    render('veterinarians/index.html.erb')
   end
 
   def show
     @veterinarian = Veterinarian.find(params[:id])
-    render('veterinarians/show.html.erb')
   end
 
   def create
     @veterinarian = Veterinarian.new(params[:veterinarian])
     if @veterinarian.save
       flash[:notice] = "#{@veterinarian.name} was added to the Pet Tracker."
-      redirect_to("/veterinarians/#{@veterinarian.id}")
+      redirect_to veterinarian_path(@veterinarian)
     else
       @veterinarians = Veterinarian.all
-      render('veterinarians/index.html.erb')
+      render 'index'
     end
   end
 
   def edit
     @veterinarian = Veterinarian.find(params[:id])
-    render('veterinarians/edit.html.erb')
   end
 
   def update
     @veterinarian = Veterinarian.find(params[:id])
     if @veterinarian.update(params[:veterinarian])
       flash[:notice] = "#{@veterinarian.name} was updated."
-      redirect_to("/veterinarians/#{@veterinarian.id}")
+      redirect_to veterinarian_path(@veterinarian)
     else
-      render('veterinarians/edit.html.erb')
+      render 'edit'
     end
   end
 
   def destroy
     @veterinarian = Veterinarian.find(params[:id])
     @veterinarian.destroy
-    render('veterinarians/destroy.html.erb')
   end
 end
