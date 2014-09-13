152c21a594bd67ba36442f8e369940a7a9f6dac1
Justin Pacubas <jpacubas@gmail.com>
2014-09-13 15:26:17 -0700

    Refactor vet_appointments routes to use resources method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'pets#index'})
-  resources :pets
-  match('pets/:pet_id/vet_appointments/new', {:via => :get, :to => 'vet_appointments#new'})
-  match('pets/:pet_id/vet_appointments', {:via => :post, :to => 'vet_appointments#create'})
-  match('pets/:pet_id/vet_appointments/:vet_appointment_id/edit', {:via => :get, :to => 'vet_appointments#edit'})
-  match('pets/:pet_id/vet_appointments/:vet_appointment_id', {:via => [:patch, :put], :to => 'vet_appointments#update'})
-  match('pets/:pet_id/vet_appointments/:vet_appointment_id', {:via => :delete, :to => 'vet_appointments#destroy'})
+  resources :pets do
+    resources :vet_appointments, :except => [:index, :show]
+  end
   match('veterinarians', {:via => :get, :to => 'veterinarians#index'})
   match('veterinarians', {:via => :post, :to => 'veterinarians#create'})
   match('veterinarians/new', {:via => :get, :to => 'veterinarians#index'})
