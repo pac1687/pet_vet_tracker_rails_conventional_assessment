2e96d70449af61557d61e891bcf64bed09dba887
Justin Pacubas <jpacubas@gmail.com>
2014-09-13 15:26:59 -0700

    Refactor veterinarians routes to use resources method.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,5 @@ Rails.application.routes.draw do
   resources :pets do
     resources :vet_appointments, :except => [:index, :show]
   end
-  match('veterinarians', {:via => :get, :to => 'veterinarians#index'})
-  match('veterinarians', {:via => :post, :to => 'veterinarians#create'})
-  match('veterinarians/new', {:via => :get, :to => 'veterinarians#index'})
-  match('veterinarians/:id', {:via => :get, :to => 'veterinarians#show'})
-  match('veterinarians/:id/edit', {:via => :get, :to => 'veterinarians#edit'})
-  match('veterinarians/:id', {:via => [:patch, :put], :to => 'veterinarians#update'})
-  match('veterinarians/:id', {:via => :delete, :to => 'veterinarians#destroy'})
+  resources :veterinarians
 end
